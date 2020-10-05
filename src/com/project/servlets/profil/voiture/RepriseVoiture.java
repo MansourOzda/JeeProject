af61df26f3e2d9bbf19f832367f37b0057678a34
justinbah <bahier.justin@gmail.com>
2020-10-05 14:21:47 +0200

    Creation des controleur

---
+++ src/com/project/servlets/profil/voiture/RepriseVoiture.java
@@ -0,0 +1,15 @@
+package com.project.servlets.profil.voiture;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RepriseVoiture extends HttpServlet {
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		/* Transmission de la paire d'objets request/response à notre JSP */
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/Profil/Voiture/RepriseVoiture.jsp" ).forward( request, response );
+	}
+}
---
+++ src/com/project/servlets/profil/voiture/Voiture.java
@@ -0,0 +1,15 @@
+package com.project.servlets.profil.voiture;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Voiture extends HttpServlet {
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		/* Transmission de la paire d'objets request/response à notre JSP */
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/Profil/Voiture/Voiture.jsp" ).forward( request, response );
+	}
+}
