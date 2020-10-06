af95df66076c71066464b8259d73b86c3133ce75
Mansour <mansour.ozdamirov@viacesi.fr>
2020-10-06 14:54:53 +0200

    Restructuration des Servlets

--- src/com/project/servlets/accueil.java
+++ src/com/project/servlets/accueil.java
@@ -1,21 +1,42 @@
 package com.project.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServlet;
 
-public class accueil extends HttpServlet {
+/**
+ * Servlet implementation class Accueil
+ */
+@WebServlet("/Accueil")
+public class Accueil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Accueil() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-		/* Transmission de la paire d'objets request/response à notre JSP */
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/index.jsp" ).forward( request, response );
+
 	}
-	
-	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
-
-
